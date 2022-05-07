25aad5aa2269db30a2cc83fa505e5e8868e02456
Positron <positron@rogers.com>
2022-05-07 17:00:17 -0400

    Add some test cases

--- tests/test.c
+++ tests/test.c
@@ -1,9 +1,57 @@
 #include <stdio.h>
+#include <string.h>
+#include <assert.h>
 
 #include "huffman.h"
 
+void test_string( const char* string );
+
 int main( void ) {
-    HUFFMAN_Construct();
-    HUFFMAN_Destruct();
-    return 0;
+   HUFFMAN_Construct();
+
+   test_string( "" );
+   test_string( "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_+" );
+   test_string(
+      "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_+\n"
+      "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_+\n"
+      "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_+\n"
+      "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_+\n"
+      "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_+\n"
+      "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_+\n"
+      "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_+\n"
+      "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_+\n"
+      "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_+\n"
+      "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_+\n"
+      "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_+\n"
+      "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_+\n"
+      "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_+\n"
+      "abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_+"
+   );
+
+   HUFFMAN_Destruct();
+   return 0;
+}
+
+void test_string( const char* string ) {
+   enum { BUFFER_SIZE = 1000 };
+
+   unsigned char encoded[ BUFFER_SIZE ];
+   unsigned char decoded[ BUFFER_SIZE ];
+
+   // We include the NUL byte in the length.
+   size_t length = strlen( string ) + 1;
+   assert( length <= BUFFER_SIZE );
+
+   memcpy( decoded, string, length );
+
+   int encodedLength = BUFFER_SIZE;
+   HUFFMAN_Encode( decoded, length, encoded, &encodedLength );
+
+   int decodedLength = BUFFER_SIZE;
+   HUFFMAN_Decode( encoded, encodedLength, decoded, &decodedLength );
+
+   assert( decodedLength == length );
+   assert( memcmp( string, decoded, decodedLength ) == 0 );
+
+   printf( "%s\n", decoded );
 }
